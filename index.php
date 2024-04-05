3dfeac709d33356067c891a2f14ef0a469674133
27122004Vasily2015 <101931202+27122004Vasily2015@users.noreply.github.com>
2024-04-05 15:26:29 +0500

    Index_main
    Главная страница проекта

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="header_nav">
+            <div class="nav_left">
+            <ul>
+                <li><a href=""><img src="images/menu.png" alt="">МЕНЮ</a></li>
+                <li><a href=""><img src="images/hurt.png" alt="">О НАС</a></li>
+                <li><a href=""><img src="images/cart.png" alt="">ТОВАРЫ</a></li>
+                <li><a href=""><img src="images/search.png" alt="">ПОИСК</a></li>
+            </ul>
+            </div>
+
+            <div class="nav_right">
+                <a href=""><img src="images/account.png" alt=""></a>
+            </div>
+        </div>
+
+        <div class="header_text">
+            <div class="header_text_left">
+                <h1></h1>
+                <h3></h3>
+                <button></button>
+            </div>
+
+            <div class="header_text_right">
+                <img src="images/battle.png" alt="">
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="main_nav">
+            <div class="main_nav_placeholder">
+                <img src="images/battle_pack" alt="">
+                <p>1 pack</p>
+            </div>
+
+            <div class="main_nav_placeholder">
+                <img src="images/battle_pack_2" alt="">
+                <p>2 pack</p>
+            </div>
+
+            <div class="main_nav_placeholder">
+                <img src="images/battle_pack_2" alt="">
+                <p>2 pack</p>
+            </div>
+
+            <div class="main_nav_placeholder">
+                <img src="images/battle_pack_2" alt="">
+                <p>2 pack</p>
+            </div>
+        </div>
+
+        <div class="main_cards">
+            <div class="main_card">
+                <div class="main_card_img">
+                    <img src="" alt="">
+                </div>
+                <div class="main_card_name">
+                <p>All New</p>
+                <h2>Cool Berry</h2>
+                </div>
+                <div class="main_card_footer">
+                <h3>₹ 200</h3>
+                <div class="plus">
+                    <a href=""><img src="images/plus.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+        <hr class="main_hr">
+
+        <div class="main_catalog">
+            <img src="images/FIZZ.png" alt="">
+            <div class="main_catalog_card">
+                    <div class="main_catalog_card_img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="main_catalog_card_name">
+                        <p>All New</p>
+                        <h2>Cool Berry</h2>
+                    </div>
+                    <div class="main_catalog_card_footer">
+                        <h3>₹ 200</h3>
+                    <div class="plus">
+                        <a href=""><img src="images/plus.png" alt=""></a>
+                    </div>
+            </div>
+
+            <div class="main_catalog_button">
+                <button>show more</button>
+            </div>
+            <hr class="main_catalog_hr">
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer_info">
+            <div class="footer_left">
+                <img src="images/FIZZ.png" alt="">
+            </div>
+
+            <div class="footer_right">
+                <div class="footer_right_name">ENJOY EVERY SIP</div>
+                <div class="footer_ridht_lastname">Our Contacts</div>
+                <div class="footer_ridht_links">
+                    <ol>
+                        <li><a href="">Cool Berry</a></li>
+                        <li><a href="">Cool Berry</a></li>
+                        <li><a href="">Cool Berry</a></li>
+                    </ol>
+                    <ol>
+                        <li><a href="">Cool Berry</a></li>
+                        <li><a href="">Cool Berry</a></li>
+                        <li><a href="">Cool Berry</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="footer_end">
+            <img src="images" alt="">
+        </div>
+    </footer>
+
+</body>
+</html>
