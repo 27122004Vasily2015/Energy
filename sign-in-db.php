acca80c55315beb652661bbd2cf6ce901cfb6899
27122004Vasily2015 <101931202+27122004Vasily2015@users.noreply.github.com>
2024-04-20 03:45:32 +0500

    Add files via upload

--- sign-in-db.php
+++ sign-in-db.php
@@ -1,15 +1,22 @@
 <?php
     require "connect-db.php";
-    $email = strip_tags(trim($_POST['email']));
+    $email = strip_tags(trim($_POST['login']));
     $pass = strip_tags(trim($_POST['pass']));
 
     $result = mysqli_fetch_assoc($result);
 
     if(empty($user)){
+        if ($email == "admin"){
+            session_start();
+
+            $_SESSION["User_id"] = $user["User_id"];
+    
+            header('Location: admin/index.php');
+        } else {
         session_start();
 
         $_SESSION["User_id"] = $user["User_id"];
 
-        header('Location: account.php');
-    }
+        header('Location: account.php');}
+    } 
 ?>
